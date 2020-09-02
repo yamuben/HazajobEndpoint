5d1d1f10ffa01c2a1a559f755eeffea1e45ae819
FabriceIRANKUNDA <irankundafabrice8@gmail.com>
2020-09-02 09:10:53 +0200

    Implementing userInfos controller and routes

--- server/utils/appError.js
+++ server/utils/appError.js
@@ -2,7 +2,7 @@ class AppErr {
   constructor(statusCode, message) {
     this.message = message;
     this.statusCode = statusCode;
-    this.status = `${statusCode}`.startWith('4') ? 'fail' : 'error';
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
   }
 }
