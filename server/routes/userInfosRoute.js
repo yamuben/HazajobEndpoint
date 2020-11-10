6ba92090f2f5e3aedb2b5a4c0c2f2e3c54d799ae
Iyamuremye Benjamin <51373376+yamuben@users.noreply.github.com>
2020-11-10 16:34:49 +0200

    Update userInfosRoute.js

--- server/routes/userInfosRoute.js
+++ server/routes/userInfosRoute.js
@@ -3,14 +3,14 @@ import * as userInfosController from '../controllers/userInfosController';
 import * as authController from '../controllers/authController';
 
 const router = express.Router();
-
-router.post('/signup', authController.signUp);
-router.post('/login', authController.login);
-router.use(authController.protect);
 router
   .route('/')
   .get(userInfosController.getAllUserInfos)
   .post(userInfosController.createUser);
+
+router.post('/signup', authController.signUp);
+router.post('/login', authController.login);
+router.use(authController.protect);
 
 router
   .route('/user-data')
